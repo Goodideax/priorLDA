1ee28910b82e951d68eae908db0223ee74a82883
goodidea <goodidea@platanus.(none)>
2013-12-20 16:28:35 -0800

    add all the files.

---
+++ src/diskarray.cpp
@@ -0,0 +1,55 @@
+#include "diskarray.h"
+#include <iostream>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+void diskArray::flush()
+{
+	int ret;
+	fseek(fp,cur_row*length*sizeof(int),SEEK_SET);
+	ret =fwrite(cached_row,sizeof(int),length,fp);
+	if(ret!=length)
+		cerr<<"error in flush"<<endl;
+}
+
+void diskArray::load(int rows)
+{
+	int ret;
+	fseek(fp,rows*length*sizeof(int),SEEK_SET);
+	ret = fread(cached_row,sizeof(int),length,fp);
+	if(ret!=length)
+		cerr<<"error in load"<<endl;
+}
+
+void diskArray::init(int rownumber)
+{
+	fp = fopen(filename.c_str(),"rwb");
+	rows = rownumber;
+}
+
+void diskArray::set_length(int len)
+{
+	length = len;
+	fseek(fp,(rows+1)*length*sizeof(int),SEEK_SET);
+	fputc('\n',fp);
+	
+}
+
+int &diskArray::visit(int i,int j)
+{
+	if(i>=0&&i<rows&&j>=0&&j<length)
+	{
+		if(cur_row!=i)
+		{
+			flush();
+			load(i);
+		}
+	}
+	else
+	{
+		cerr<<"error in visit "<<i<<' '<<j<<endl;
+		return cached_row[0];
+	}
+	return cached_row[j];
+}
---
+++ src/diskarray.h
@@ -0,0 +1,40 @@
+#ifndef _DISKARRAY_H
+#define _DISKARRAY_H
+
+#include <string>
+using namespace std;
+class diskArray
+{
+	//storage file name
+	string filename;
+	FILE * fp;
+
+	//the row and column number
+        int rows;
+        int length;
+
+	//in memory cache
+	int * cached_row;
+
+	//the current row number;
+	int cur_row;
+
+	//flush the cache to disk;
+	void flush();
+
+	//load a row from the disk
+	void load(int rows);
+public:
+	//set the row and column number
+        void init(int rownumber);
+	void set_length(int len);
+
+	//visit an element
+	int &visit(int i,int j);
+	diskArray()
+	{
+		filename="array.tmp";
+		cur_row = -1;
+	}	
+};
+#endif
